94e3c9862650f2da456e4bf08a617d2ad528432b
Dr4g0 <d_d0003@yahoo.com>
2015-03-16 12:33:51 +0200

    Changed architecture of the project, add datepicker

--- js/calculator.js
+++ js/calculator.js
@@ -1,35 +1,40 @@
 $(document).ready(function () {
-    $('input[name="calc-sum"]').keypress(function () {
-        return isKeyPartOfDecimalNumber(event);
-    })
+    $('#calculator').addClass('container-fluid').load('vendor/calculator.html', function () {
+        $('input[name="calc-sum"]').keypress(function () {
+            return isKeyPartOfDecimalNumber(event);
+        })
 
-    $('#btn-reset').click(function () {
-        resetForm($('#calc-form'));
-    })
+        $('#btn-reset').click(function () {
+            resetForm($('#calc-form'));
+        })
 
-    $('#btn-calc').click(function () {
-        calculate();
-    })
+        $('#btn-calc').click(function () {
+            calculate();
+        })
 
-    $('.date-form').keypress(function () {
-        return checkDateFormat(event);
-    })
+        $('.date-form').keypress(function () {
+            return checkDateFormat(event);
+        })
 
+        $('.datetimepicker').datetimepicker({
+            format: "DD.MM.YYYY"
+        });
+    });
 })
 
 var monthsWith31Days = [1, 3, 5, 7, 8, 10, 12];
-var monthsWith30Days = [4, 6, 9, 11];
+var fadeOutTime = 4000;
 
 function isKeyPartOfDecimalNumber(evt) {
     var inputFormValue = evt.target.value;
     var charCode = (evt.which) ? evt.which : event.keyCode
     if (charCode > 31 && (charCode !== 46 && (charCode < 48 || charCode > 57))) {
-        $('#error-sum-info').show();
+        $('#error-sum-info').show().fadeOut(fadeOutTime);
         return false;
     }
     if (charCode === 46) {
         if (inputFormValue.indexOf('.') > -1) {
-            $('#error-sum-info').show();
+            $('#error-sum-info').show().fadeOut(fadeOutTime);
             return false;
         }
     }
@@ -42,16 +47,18 @@ function resetForm($form) {
     $form.find('input:radio, input:checkbox')
         .removeAttr('checked').removeAttr('selected');
     var selectControls = $form.find('select');
-    selectControls.each(
-        function () {
-            $(this).val($("option:first", this).val());
-        })
+    selectControls.each(function () {
+        $(this).val($("option:first", this).val());
+    })
     $('#error-sum-info').hide();
     $('.error-date-info').hide();
+    $('#calc-result').hide();
 }
 
 function calculate() {
-
+    if (datesValidate()) {
+        $('#calc-result').show();
+    }
 }
 
 function checkDateFormat(evt) {
@@ -61,7 +68,7 @@ function checkDateFormat(evt) {
     var dateErrorInfo = $(evt.target).parent().parent().next();
 
     if (charCode > 31 && (charCode !== 46 && (charCode < 48 || charCode > 57))) {
-        dateErrorInfo.text('Въведете само цифри и точка "." за разделител').show();
+        showDateError(evt.target, 'Въведете само цифри и точка "." за разделител');
         return false;
     }
     if (charCode !== 46) {
@@ -71,7 +78,7 @@ function checkDateFormat(evt) {
                 || (day.length === 1 && parseInt(day) > 3)
                 || parseInt(day) > 31
                 || (day.length === 2 && parseInt(day) == 0)) {
-                dateErrorInfo.text('Опитвате се да въведете невалиден ден от месеца').show();
+                showDateError(evt.target, 'Опитвате се да въведете невалиден ден от месеца');
                 return false;
             }
         }
@@ -81,18 +88,18 @@ function checkDateFormat(evt) {
                 || (month.length === 1 && parseInt(month) > 1)
                 || parseInt(month) > 12
                 || (month.length === 2 && parseInt(month) == 0)) {
-                dateErrorInfo.text('Опитвате се да въведете невалиден месец').show();
+                showDateError(evt.target, 'Опитвате се да въведете невалиден месец');
                 return false;
             }
             if (month.length === 2) {
                 var day = inputFormValue.substring(0, 2);
                 if (parseInt(day) > 29 && parseInt(month) === 2) {
-                    dateErrorInfo.text('Февруари може да съдържа максимум 29 дена').show();
+                    showDateError(evt.target, 'Февруари може да съдържа максимум 29 дена');
                     return false;
                 }
 
                 if (parseInt(day) === 31 && monthsWith31Days.indexOf(parseInt(month)) === -1) {
-                    dateErrorInfo.text('Месецът не съдържа 31 дена').show();
+                    showDateError(evt.target, 'Месецът не съдържа 31 дена');
                     return false;
                 }
             }
@@ -102,12 +109,12 @@ function checkDateFormat(evt) {
             var month = inputFormValue.substring(3, 5);
             var year = inputFormValue.substring(6) + String.fromCharCode(charCode);
             if (year.length > 4) {
-                dateErrorInfo.text('Опитвате се да въведете невалидна година').show();
+                showDateError(evt.target, 'Опитвате се да въведете невалидна година');
                 return false;
             }
             if ((year.length === 1 && (parseInt(year) < 1 || parseInt(year) > 2))
                 || (year.length === 2 && (parseInt(year) < 19 ))) {
-                dateErrorInfo.text('Калкулаторът работи с дати между 01.01.1900 до 31.12.2999').show();
+                showDateError(evt.target, 'Калкулаторът работи с дати между 01.01.1900 до 31.12.2999');
                 return false;
             }
             if (year.length == 4) {
@@ -122,17 +129,17 @@ function checkDateFormat(evt) {
     }
     else {
         if (inputFormValue.length < 2) {
-            dateErrorInfo.text('Опитвате се да въведете невалиден ден от месеца').show();
+            showDateError(evt.target, 'Опитвате се да въведете невалиден ден от месеца');
             return false;
         }
 
         if (inputFormValue.length > 2 && inputFormValue.length < 5) {
-            dateErrorInfo.text('Опитвате се да въведете невалиден месец').show();
+            showDateError(evt.target, 'Опитвате се да въведете невалиден месец');
             return false;
         }
 
         if (decimalPointsCount === 2) {
-            dateErrorInfo.text('Опитвате се да въведете невалидна година').show();
+            showDateError(evt.target, 'Опитвате се да въведете невалидна година');
             return false;
         }
     }
@@ -143,4 +150,25 @@ function checkDateFormat(evt) {
 
 function isLeapYear(year) {
     return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+}
+
+function datesValidate() {
+    var dateInputs = $('.date-form');
+    var isAllDatesValid = true;
+    dateInputs.each(function () {
+        var dateErrorInfo = $(this).parent().parent().next();
+        if ($(this).val().length !== 10) {
+            showDateError(this, 'Моля, въведете коректна дата');
+            isAllDatesValid = false;
+        }
+    })
+    if (isAllDatesValid) {
+        return true;
+    }
+    return false;
+}
+
+function showDateError(eventElement, message) {
+    var dateErrorInfo = $(eventElement).parent().parent().next();
+    dateErrorInfo.text(message).show().fadeOut(fadeOutTime);
 }
